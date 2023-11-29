6809f3f611954517bb1071a158b1594cc8d8e82b
baizhi958216 <1475289190@qq.com>
2023-11-30 07:58:16 +0800

    feat: sse
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1 @click="sendEvent">Click to Send Message</h1>
+    <ul>
+      <li v-for="message in messages">{{ message }}</li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+const messages = ref<string[]>([])
+onMounted(() => {
+  setupSSE();
+})
+const setupSSE = () => {
+  // 连接 SSE 服务器
+  const eventSource = new EventSource('http://localhost:8080/sse');
+  // 处理接收到的消息
+  eventSource.onmessage = (event) => {
+    messages.value.push(event.data);
+  };
+  // 处理错误
+  eventSource.onerror = (error) => {
+    console.error('SSE Error:', error);
+    eventSource.close();
+  };
 }
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+const sendEvent = () => {
+  fetch(`http://localhost:8080/sse/sendEvent`)
 }
-</style>
+</script>
