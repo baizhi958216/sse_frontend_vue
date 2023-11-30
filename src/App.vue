5af5f61abd99a44f5b2155acabd754d3774cc5a7
baizhi958216 <1475289190@qq.com>
2023-11-30 10:20:25 +0800

    feat: 通知计数
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <div>
-    <h1 @click="sendEvent">Click to Send Message</h1>
-    <ul>
+  <div style="display: flex;justify-content: center;align-items: center">
+    <ul style="height:600px;overflow-y:auto;">
       <li v-for="message in messages">{{ message }}</li>
     </ul>
+    <div style="font-size: 200px;position: relative">🔔
+      <div style="position: absolute;right: 60px;top: 60px; width: 64px;height: 64px;border-radius: 50%;background: red;display: flex;justify-content: center;align-items: center;font-size: 32px">
+        {{ messageCount }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
-
 const messages = ref<string[]>([])
+const messageCount = ref<number>(0)
 onMounted(() => {
   setupSSE();
 })
@@ -20,14 +24,12 @@ const setupSSE = () => {
   // 处理接收到的消息
   eventSource.onmessage = (event) => {
     messages.value.push(event.data);
+    messageCount.value+=1
   };
   // 处理错误
   eventSource.onerror = (error) => {
     console.error('SSE Error:', error);
     eventSource.close();
   };
-}
-const sendEvent = () => {
-  fetch(`http://localhost:8080/sse/sendEvent`)
 }
 </script>
