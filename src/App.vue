c251bea6fd7a7cf4f1acc5274a95933b23965c98
baizhi958216 <1475289190@qq.com>
2023-12-01 12:39:16 +0800

    feat: 监听elysia
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
-  <div style="display: flex;justify-content: center;align-items: center">
-    <ul style="height:600px;overflow-y:auto;">
+  <div style="display: flex; justify-content: center; align-items: center">
+    <ul style="height: 600px; overflow-y: auto">
       <li v-for="message in messages">{{ message }}</li>
     </ul>
-    <div style="font-size: 200px;position: relative">🔔
-      <div style="position: absolute;right: 60px;top: 60px; width: 64px;height: 64px;border-radius: 50%;background: red;display: flex;justify-content: center;align-items: center;font-size: 32px">
+    <div style="font-size: 200px; position: relative">
+      🔔
+      <div
+        style="
+          position: absolute;
+          right: 60px;
+          top: 60px;
+          width: 64px;
+          height: 64px;
+          border-radius: 50%;
+          background: red;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 32px;
+        "
+      >
         {{ messageCount }}
       </div>
     </div>
@@ -12,24 +27,29 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-const messages = ref<string[]>([])
-const messageCount = ref<number>(0)
+import { onMounted, ref } from "vue";
+const messages = ref<string[]>([]);
+const messageCount = ref<number>(0);
 onMounted(() => {
   setupSSE();
-})
+});
 const setupSSE = () => {
   // 连接 SSE 服务器
-  const eventSource = new EventSource('http://localhost:8080/sse');
-  // 处理接收到的消息
-  eventSource.onmessage = (event) => {
-    messages.value.push(event.data);
-    messageCount.value+=1
+  const eventSource = new EventSource("http://localhost:8080/sse");
+  // 监听服务器发送的事件
+  eventSource.addEventListener("elysia", function (message) {
+    messages.value.push(message.data);
+    messageCount.value++;
+  });
+
+  eventSource.onerror = function (error) {
+    // 处理连接错误
+    console.error("Error occurred:", error);
   };
-  // 处理错误
-  eventSource.onerror = (error) => {
-    console.error('SSE Error:', error);
+
+  // 在页面关闭或不需要连接时，记得关闭EventSource
+  window.onbeforeunload = function () {
     eventSource.close();
   };
-}
+};
 </script>
